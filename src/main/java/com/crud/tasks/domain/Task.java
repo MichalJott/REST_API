94cf176f48bccaf34faaafbdb903a0f80f15fcbd
MichalJott <MichalJott@github.com>
2020-06-21 11:25:46 +0200

    19.2_FindTaskdByIdInDatabase

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,8 +1,25 @@
 package com.crud.tasks.domain;
 
-public class Task {
-    private Long id;
-    private String title;
-    private String content;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+
+@Entity(name = "tasks" )
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String title ;
+
+    @Column(name = "description")
+    private String content;
 }
--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -1,14 +1,14 @@
 package com.crud.tasks.domain;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+
 public class TaskDto {
     private Long id;
-    private String title;
+    private String title ;
     private String content;
 
 }
